6f875c7f5c88c40fe3ec7f2446482eb2e2666a6a
Sangow <sergey.anpilogich@gmail.com>
2019-05-03 10:37:38 +0300

    auto-modified files from NOUT

--- w3/dz/arrayLibrary/main.c
+++ w3/dz/arrayLibrary/main.c
@@ -2,6 +2,30 @@
 
 #include "array.h"
 
+#define MAX_SIZE 100
+
 void main() {
-    printf("EPIC WIN\n");
+    FILE *in = fopen("task.in", "r");
+    FILE *out = fopen("task.out", "w");
+    int array[MAX_SIZE];
+    int size = arrayScan(in, array, MAX_SIZE);
+
+    fprintf(out, "arrayPrint()\n");
+    arrayPrint(out, array, size);
+    fprintf(out, "\n");
+
+    fprintf(out, "arrayIncrement()\n");
+    arrayIncrement(array, size, 2);
+    arrayPrint(out, array, size);
+    fprintf(out, "\n");
+
+    fprintf(out, "arrayAbs()\n");
+    arrayAbs(array, size);
+    arrayPrint(out, array, size);
+    fprintf(out, "\n");
+
+    fprintf(out, "arrayShift()\n");
+    arrayShift(array, size, 3);
+    arrayPrint(out, array, size);
+    fprintf(out, "\n");
 }

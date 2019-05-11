9a4aad9127c02b7297fedf94f9507acb15e6a6b2
Sangow <sergey.anpilogich@gmail.com>
2019-05-11 07:21:36 +0300

    added some funcs dz_cpp/unit

--- w3/dz_cpp/bcw4/bcw4/Unit/Unit.cpp
+++ w3/dz_cpp/bcw4/bcw4/Unit/Unit.cpp
@@ -8,9 +8,58 @@ void Unit::ensureIsAlive() {
 }
 
 Unit::Unit(const std::sting& name, int hp, int dmg) {
-    ensureIsAlive();
+    this->ensureIsAlive();
     this->damage = dmg;
     this->hitPoints = hp;
     this->hitPointsLimit = hp;
     this>name = name;
 }
+
+Unit::~Unit() {}
+
+int Unit::getDamage() const {
+    return this->damage;
+}
+
+int Unit::getHitPoints() const {
+    return this->hitPoints;
+}
+
+int Unit::getHitPointsLimit() const {
+    return this->hitPointsLimit;
+}
+
+const std::sting& Unit::getName() const {
+    return this->name;
+}
+
+void Unit::addHitPoints(int hp) {
+    this->ensureIsAlive();
+
+    if( hp > this->hitPointsLimit - this->hitPoints ) {
+        this->hitPoints = this->hitPointsLimit;
+        return;
+    }
+    this->hitPoints += hp;
+}
+
+void Unit::takeDamage(int dmg) {
+    this->ensureIsAlive();
+
+    if ( dmg > this->hitPoints ) {
+        this->hitPoints = 0;
+        return;
+    }
+    this->hitPoints -= dmg;
+}
+
+void Unit::attack(Unit& enemy) {
+    // enemy.ensureIsAlive(); ??
+    enemy.takeDamage(this->damage);
+    enemy.ensureIsAlive();
+
+    enemy.counterAttack(*this);
+}
+ void Unit::counterAttack(Unit& enemy) {
+
+ }

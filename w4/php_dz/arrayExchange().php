c5f28658935a6360f482419d7bb6938628a59fb5
Sangow <sergey.anpilogich@gmail.com>
2019-06-13 18:15:48 +0300

    <<rabota>>[master] done arrayExchange().php

---
+++ w4/php_dz/arrayExchange().php
@@ -0,0 +1,22 @@
+<?php
+    function arrayExchange(&$array, $len) {
+        for ( $i = 0, $size = $len - $len / 2; $i < $size; $i += 2 ) {
+            $tmp = $array[$i];
+
+            $array[$i] = $array[$i+1];
+            $array[$i+1] = $tmp;
+        }
+    }
+
+    $array = array( '0' => 1,
+                    '1' => 2,
+                    '2' => 100,
+                    '3' => -10,
+                    '4' => 0,
+                    '5' => 9,
+                    '6' => 55);
+
+    arrayExchange($array, 6) . PHP_EOL;
+
+    var_dump($array);
+?>

c2f023cdd7b97de76d54ee8112fb6e27865024a2
Sangow <sergey.anpilogich@gmail.com>
2019-08-27 01:35:56 +0300

    <<home>>[master] working on spellcasters

---
+++ w4/army/spellcaster/SpellCaster.h
@@ -0,0 +1,14 @@
+#ifndef SPELLCASTER_H
+#define SPELLCASTER_H
+
+#include <iostream>
+#include "../unit/Unit.h"
+#include "../state/MagicState.h"
+
+class SpellCaster : public Unit {
+    
+    public:
+
+}
+
+#endif // SPELLCASTER_H

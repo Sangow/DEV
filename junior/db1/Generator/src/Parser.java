cd5af035844e5889d42fde5b9458bbfe33977b9c
Sangow <sergey.anpilogich@gmail.com>
2019-12-15 00:29:53 +0200

    Added some files. kek

---
+++ junior/db1/Generator/src/Parser.java
@@ -0,0 +1,47 @@
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.StringJoiner;
+
+class YamlReader {
+
+    public Map<String, Map<String, Map<String, String>>> read(String path) throws IOException {
+        InputStream in = Files.newInputStream(Paths.get(path));
+        Yaml yaml = new Yaml();
+        return (Map<String, Map<String, Map<String, String>>>) yaml.load(in);
+    }
+}
+
+public class Parser {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> statements = new ArrayList<>();
+        StringBuilder sb;
+        YamlReader reader = new YamlReader();
+
+        Map<String, Map<String, Map<String, String>>> config = reader.read("db.yml");
+
+        for(Map.Entry<String, Map<String, Map<String, String>>> entry0 : config.entrySet()) {
+            sb = new StringBuilder();
+
+            for ( Map.Entry<String, Map<String, String>> entry1 : entry0.getValue().entrySet() ) {
+                StringJoiner sj = new StringJoiner(", ", "", "");
+
+                for ( Map.Entry<String, String> entry2 : entry1.getValue().entrySet() ) {
+                    sj.add(entry2.getKey() + " " + entry2.getValue());
+                }
+                statements.add(sb.append("CREATE TABLE \"" + entry0.getKey() + "\" ( " + sj + " );").toString());
+            }
+
+        }
+
+        for ( String s : statements ) {
+            System.out.println(s);
+        }
+
+    }
+}

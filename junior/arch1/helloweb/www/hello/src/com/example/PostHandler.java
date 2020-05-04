3b912da514392fd86f56e50aee66f84e7f66c86a
Sangow <sangow@Sangows-MacBook-Pro.local>
2020-05-04 14:20:44 +0300

    [post_handler] imported ready to use form.html and all included staff to this

---
+++ junior/arch1/helloweb/www/hello/src/com/example/PostHandler.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+    public class PostHandler extends HttpServlet {
+        public void doGet(HttpServletRequest request,
+                            HttpServletResponse response)
+                                throws IOException, ServletException {
+        }
+}
